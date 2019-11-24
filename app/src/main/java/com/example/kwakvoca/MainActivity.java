dec8433ff4b3b4d0d999f0710cb7d060bdbaab75
Kwak04 <tronixdevelop@gmail.com>
2019-11-25 00:30:36 +0900

    feat(UI): Added title and buttons

--- app/src/main/java/com/example/kwakvoca/MainActivity.java
+++ app/src/main/java/com/example/kwakvoca/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.kwakvoca;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
