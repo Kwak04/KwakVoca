237dc076da75050ed0d6aa2d6bc4f199c01c13be
Kwak04 <tronixdevelop@gmail.com>
2019-11-25 00:30:22 +0900

    feat(UI): Added title and buttons

--- app/src/main/java/com/example/kwakvoca/MainActivity.java
+++ app/src/main/java/com/example/kwakvoca/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.kwakvoca;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getWindow().setStatusBarColor(getApplicationContext().getColor(R.color.colorBackground));
     }
 }
