c4cfc9b3229ed4e1eccf7444d63b7375dc02a7c2
Kwak04 <tronixdevelop@gmail.com>
2019-11-27 00:59:25 +0900

    feat(UI): Made splash screen

--- app/src/main/java/com/example/kwakvoca/SplashActivity.java
+++ app/src/main/java/com/example/kwakvoca/SplashActivity.java
@@ -2,7 +2,9 @@ package com.example.kwakvoca;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +12,22 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        getWindow().setStatusBarColor(getApplicationContext().getColor(R.color.colorBackground));
+
+        Handler hd = new Handler();
+        hd.postDelayed(new SplashHandler(), 1000);
     }
+
+    private class SplashHandler implements Runnable {
+        public void run(){
+            final Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            overridePendingTransition(0, R.anim.fade_out);
+            SplashActivity.this.finish();
+        }
+    }
+
+    @Override
+    public void onBackPressed() { }
 }
