99ca40b8dbfa86814f2fab3d8b74431a2bc1aa9a
Tronix Kwak <tronixdevelop@gmail.com>
2019-11-27 01:02:52 +0900

    feat(UI): Made splash screen (#5)
    * feat(UI): Designed splash screen
    
    * feat(UI): Made splash screen

---
+++ app/src/main/java/com/example/kwakvoca/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.example.kwakvoca;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        getWindow().setStatusBarColor(getApplicationContext().getColor(R.color.colorBackground));
+
+        Handler hd = new Handler();
+        hd.postDelayed(new SplashHandler(), 1000);
+    }
+
+    private class SplashHandler implements Runnable {
+        public void run(){
+            final Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            overridePendingTransition(0, R.anim.fade_out);
+            SplashActivity.this.finish();
+        }
+    }
+
+    @Override
+    public void onBackPressed() { }
+}
