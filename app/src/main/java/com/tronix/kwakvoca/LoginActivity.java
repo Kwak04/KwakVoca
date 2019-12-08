7537888095818a478fdaf73ffe343ca63451578a
Kwak04 <tronixdevelop@gmail.com>
2019-12-09 00:56:54 +0900

    feat: Made Login activity

---
+++ app/src/main/java/com/tronix/kwakvoca/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.tronix.kwakvoca;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        getWindow().setStatusBarColor(getApplicationContext().getColor(R.color.colorBackground));
+    }
+}
