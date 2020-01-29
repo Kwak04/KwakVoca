4a8e141a9da33c0271f28a3b8eb6350058ae6ec2
Kwak04 <tronixdevelop@gmail.com>
2020-01-29 13:55:45 +0900

    feat: Move to releases page when click the update button

--- app/src/main/java/com/tronix/kwakvoca/UpdateActivity.java
+++ app/src/main/java/com/tronix/kwakvoca/UpdateActivity.java
@@ -1,16 +1,34 @@
 package com.tronix.kwakvoca;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class UpdateActivity extends AppCompatActivity {
+
+    final String UPDATE_URL = "https://github.com/Kwak04/KwakVoca/releases";
+    Button update;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update);
 
+        update = findViewById(R.id.btn_update);
+
         getWindow().setStatusBarColor(getColor(R.color.colorBackground));
+
+        // Update button
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(UPDATE_URL));
+                startActivity(intent);
+            }
+        });
     }
 }
