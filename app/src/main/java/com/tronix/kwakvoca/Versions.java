2422c6f0b6a036887614f00943f3016ab2c804da
Kwak04 <tronixdevelop@gmail.com>
2020-03-16 20:51:30 +0900

    chore: Updated version code

--- app/src/main/java/com/tronix/kwakvoca/Versions.java
+++ app/src/main/java/com/tronix/kwakvoca/Versions.java
@@ -2,8 +2,8 @@ package com.tronix.kwakvoca;
 
 final class Versions {
 
-    static final String CURRENT_VERSION = "v0.4";
-    static final int CURRENT_VERSION_CODE = VERSION_CODES.V0_4;
+    static final String CURRENT_VERSION = "v0.4.1";
+    static final int CURRENT_VERSION_CODE = VERSION_CODES.V0_4_1;
 
     public final static class VERSION_CODES {
         static final int V0_1 = 1;
@@ -12,5 +12,6 @@ final class Versions {
         static final int V0_2_2 = 4;
         static final int V0_3 = 5;
         static final int V0_4 = 6;
+        static final int V0_4_1 = 7;
     }
 }
